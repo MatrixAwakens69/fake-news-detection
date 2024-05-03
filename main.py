a963e4484c6e423fb00aeab1b8dfd4422134e5ae
ponnanna123 <21cs02003@iitbbs.ac.in>
2024-05-03 19:41:09 +0530

    complete main.py

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ df = pd.read_csv('E:\\GitHub\\fake-news-detection\\dataset\\news.csv')
 
 print(df.shape)
 print(df.head())
+
+labels = df.label
+labels.head()
+
+x_train, x_test, y_train, y_test = train_test_split(df['text'], labels, test_size=0.05, random_state=7)
+
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+
+tfidf_train = tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test = tfidf_vectorizer.transform(x_test)
+
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+
+y_pred = pac.predict(tfidf_test)
+score = accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
