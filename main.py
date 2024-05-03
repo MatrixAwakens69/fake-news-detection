e4aa05adf463c6e8e1b691900139ef8ac743c7b5
ponnanna123 <21cs02003@iitbbs.ac.in>
2024-05-03 19:19:42 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+df = pd.read_csv('E:\\GitHub\\fake-news-detection\\dataset\\news.csv')
+
+print(df.shape)
+print(df.head())
